3aa6dc489fce4c30f55170a5b18b002ee7bacf96
Carlos Castillo <cookieo9@gmail.com>
2023-12-11 12:48:42 -0800

    feat: update Simulate + tests
    Update Simulate to take a sequence to add to. This allows 2 things:
     - Simulate can become a method
     - We can simulate multi-step algorithms with much less friction
    
    The previous Simulate is now Simulation, and tests have been added
    to ensure that Single(initial).Simulate(step) is logically equivalent
    to Simulation(initial,step) and it simplifies the case where there
    is only a single simulation step.

--- simulate_test.go
+++ simulate_test.go
@@ -4,34 +4,84 @@ import (
 	"testing"
 
 	"github.com/cookieo9/sequence/tools"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestSimulate(t *testing.T) {
-	seq := Simulate(2, func(i int) (int, bool) {
-		i *= i
-		return i, i < 1_000_000
-	})
-	want := New(2, 4, 16, 256, 65536)
+	start := MakePair(0, 1)
+	sim := func(p Pair[int, int]) (Pair[int, int], bool) {
+		a, b := p.B(), p.A()+p.B()
+		p = MakePair(a, b)
+		return p, p.B() < 1_000
+	}
+	seq := Simulation(start, sim)
+	seq2 := Single(start).Simulate(sim)
 
-	compareSequences(t, seq, want)
+	want := New[Pair[int, int]]([]Pair[int, int]{
+		{0, 1}, {1, 1}, {1, 2}, {2, 3}, {3, 5}, {5, 8}, {8, 13}, {13, 21}, {21, 34},
+		{34, 55}, {55, 89}, {89, 144}, {144, 233}, {233, 377}, {377, 610}, {610, 987},
+	}...)
+
+	opts := []cmp.Option{cmpopts.EquateComparable(Pair[int, int]{})}
+
+	seqL4 := seq.Limit(4)
+	seq2L4 := seq2.Limit(4)
+	wantL4 := want.Limit(4)
+
+	var testCases = []struct {
+		name string
+		got  Sequence[Pair[int, int]]
+		want Sequence[Pair[int, int]]
+		opts []cmp.Option
+	}{
+		{name: "Simulation", got: seq, want: want, opts: opts},
+		{name: "Single+Simulate", got: seq2, want: want, opts: opts},
+		{name: "Compare-Sims", got: seq, want: seq2, opts: opts},
+
+		{name: "Simulation+Limit(4)", got: seqL4, want: wantL4, opts: opts},
+		{name: "Single+Simulate+Limit(4)", got: seq2L4, want: wantL4, opts: opts},
+		{name: "Compare-Sims+Limit(4)", got: seqL4, want: seq2L4, opts: opts},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, sequenceCompareTest(tc.got, tc.want, tc.opts...))
+	}
 }
 
 func BenchmarkSimulate(b *testing.B) {
-	seq := Simulate(2, func(i int) (int, bool) {
+	start := 2
+	sim := func(i int) (int, bool) {
 		i *= i
 		return i, i < 1_000_000
-	})
+	}
 
-	b.Run("Raw", func(b *testing.B) {
+	seq := Simulation(start, sim)
+	b.Run("Simulation", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			_ = tools.Must(seq.Last())
 		}
 	})
 
+	seq2 := Single(start).Simulate(sim)
+	b.Run("Single+Simulate", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = tools.Must(seq2.Last())
+		}
+	})
+
 	mSeq := seq.Materialize()
-	b.Run("Materialized", func(b *testing.B) {
+	b.Run("Simulation+Materialize", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			_ = tools.Must(mSeq.Last())
+		}
+	})
+
+	mSeq2 := seq2.Materialize()
+	b.Run("Single+Simulate+Materialize", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = tools.Must(mSeq2.Last())
 		}
 	})
 }
